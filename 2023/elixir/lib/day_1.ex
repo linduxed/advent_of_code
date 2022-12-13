2cf591b368df916345ebff5239d20f986cc5ed78
Mikael Muszynski <mikael@muszynski.se>
2022-12-13 15:32:34 +0100

    2023 day 1 (elixir)

--- 2023/elixir/lib/day_1.ex
+++ 2023/elixir/lib/day_1.ex
@@ -1,9 +1,78 @@
 defmodule AdventOfCode2023.Day1 do
+  @digits 0..9
+  @digit_strings @digits |> Enum.map(&Integer.to_string/1)
+
   def part_1(lines) do
     lines
+    |> Enum.map(fn line ->
+      line
+      |> String.codepoints()
+      |> Enum.filter(&(&1 in @digit_strings))
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(&join_last_and_first_number/1)
+    |> Enum.sum()
   end
 
   def part_2(lines) do
+    digit_string_translations = Enum.zip(@digit_strings, @digits) |> Enum.into(%{})
+
+    number_words = ~w(zero one two three four five six seven eight nine)
+    number_word_translations = Enum.zip(number_words, @digits) |> Enum.into(%{})
+
+    number_string_translations =
+      Map.merge(
+        digit_string_translations,
+        number_word_translations
+      )
+
+    digit_strings_or_number_words = Map.keys(number_string_translations)
+
     lines
+    |> Enum.map(fn line ->
+      line
+      |> String.codepoints()
+      # Reversal of characters in string only because lambda in `scan` does
+      # element prepending. Either the reversal is done here, or each of the
+      # lists built by the `scan` would need to be reversed.
+      |> Enum.reverse()
+      # Traverse all of the characters (codepoints) in each line, starting
+      # from the end, to create substrings that start from the end and
+      # progressively build up to all the characters in the line. This is done
+      # to enable matching at the start of each of the substrings, since the
+      # number words may be overlapping in the lines.
+      |> Enum.scan([], fn character, previous_characters ->
+        [character | previous_characters]
+      end)
+      # Build substrings from the characters.
+      |> Enum.map(&Enum.join/1)
+      # Reverse again, to have the full line "substring" at the start, so that
+      # one can match from the start of the line.
+      |> Enum.reverse()
+      # Find the substrings that start with one of the string-form digits or
+      # number words, and then replace that substring with the number that
+      # corresponds to the start of the substring. Reject the other substrings.
+      |> Enum.flat_map(fn substring ->
+        matching_string =
+          Enum.find(digit_strings_or_number_words, fn digit_or_number_word ->
+            String.starts_with?(substring, digit_or_number_word)
+          end)
+
+        if matching_string do
+          [Map.fetch!(number_string_translations, matching_string)]
+        else
+          []
+        end
+      end)
+    end)
+    |> Enum.map(&join_last_and_first_number/1)
+    |> Enum.sum()
+  end
+
+  defp join_last_and_first_number(numbers) do
+    [List.first(numbers), List.last(numbers)]
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join()
+    |> String.to_integer()
   end
 end
--- 2023/elixir/test/day_1_test.exs
+++ 2023/elixir/test/day_1_test.exs
@@ -5,14 +5,39 @@ defmodule AdventOfCode2020.Day1Test do
   alias AdventOfCode2023.Day1
 
   describe "part_1/1" do
-    test "description" do
-      :ok
+    test "find the sum of the numbers formed on each line by the first and " <>
+           "last digit" do
+      lines = ~w(
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+      )
+
+      assert Day1.part_1(lines) == 142
     end
   end
 
   describe "part_2/1" do
-    test "description" do
-      :ok
+    test "handles lines with overlapping words" do
+      lines = ["eightwothree"]
+
+      assert Day1.part_2(lines) == 83
+    end
+
+    test "find the sum of the groups of numbers that add up to the top three " <>
+           "greatest sums" do
+      lines = ~w(
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+      )
+
+      assert Day1.part_2(lines) == 281
     end
   end
 end
