2236d929c6b3534e99934eb581248633edd0adc4
Mikael Muszynski <mikael@muszynski.se>
2022-12-13 15:35:35 +0100

    Change function names to have numbers in them
    Easier to type when calling the functions in `iex`.

--- 2022/elixir/lib/day_1.ex
+++ 2022/elixir/lib/day_1.ex
@@ -1,12 +1,12 @@
 defmodule AdventOfCode2022.Day1 do
-  def part_one(lines) do
+  def part_1(lines) do
     lines
     |> number_lines_to_integer_groups()
     |> Enum.max_by(&Enum.sum/1)
     |> Enum.sum()
   end
 
-  def part_two(lines) do
+  def part_2(lines) do
     lines
     |> number_lines_to_integer_groups()
     |> Enum.sort_by(&Enum.sum/1, :desc)
--- 2022/elixir/test/day_1_test.exs
+++ 2022/elixir/test/day_1_test.exs
@@ -21,17 +21,17 @@ defmodule AdventOfCode2020.Day1Test do
         "10000"
       ]
 
-  describe "part_one/1" do
+  describe "part_1/1" do
     test "find the sum of the group of numbers that adds up to the " <>
            "greatest sum" do
-      assert Day1.part_one(@lines) == 24000
+      assert Day1.part_1(@lines) == 24000
     end
   end
 
-  describe "part_two/1" do
+  describe "part_2/1" do
     test "find the sum of the groups of numbers that add up to the top " <>
            "three greatest sums" do
-      assert Day1.part_two(@lines) == 45000
+      assert Day1.part_2(@lines) == 45000
     end
   end
 end
