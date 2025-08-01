79e9d64d990b3f1b23f3763b7421e360c22dc0ce
Mikael Muszynski <linduxed@gmail.com>
2025-08-01 20:06:43 +0200

    Move 2015 days into language hierarchy of later years

---
+++ 2015/elixir/lib/advent_of_code_2015.ex
@@ -0,0 +1,7 @@
+defmodule AdventOfCode2015 do
+  def read_input_for_day(day_number, opts \\ []) do
+    "input/day_#{day_number}.txt"
+    |> File.read!()
+    |> String.split("\n", trim: opts[:trim])
+  end
+end
---
+++ 2015/elixir/lib/day_7.ex
@@ -0,0 +1,7 @@
+defmodule AdventOfCode2015.Day7 do
+  def part_1(lines) do
+  end
+
+  def part_2(lines) do
+  end
+end
