c1cc13286e80b024362ed5df2713951817112330
Mikael Muszynski <linduxed@gmail.com>
2025-08-01 22:50:14 +0200

    Add template for elixir year

---
+++ templates/elixir/lib/day_1.ex
@@ -0,0 +1,7 @@
+defmodule AdventOfCode2015.Day1 do
+  def part_1(lines) do
+  end
+
+  def part_2(lines) do
+  end
+end
---
+++ templates/elixir/test/day_1_test.exs
@@ -0,0 +1,24 @@
+defmodule AdventOfCode2015.Day1Test do
+  use ExUnit.Case
+  require ExUnitProperties
+
+  alias AdventOfCode2015.Day1
+
+  describe "part_1/1" do
+    test "description" do
+      lines = ~w(
+      )
+
+      assert Day1.part_1(lines) == :foobar
+    end
+  end
+
+  describe "part_2/1" do
+    test "description" do
+      lines = ~w(
+      )
+
+      assert Day1.part_2(lines) == :foobar
+    end
+  end
+end
