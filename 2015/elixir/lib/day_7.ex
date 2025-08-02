e694b92886f25e502bd022d7fc82ab4bf5aa1bbf
Mikael Muszynski <linduxed@gmail.com>
2025-08-02 08:40:39 +0200

    2015 - day 7 (elixir)
    Memoization seemed to be absolutely essential to have the program
    execute in a reasonable amount of time. I started off not having
    memoization, and it ran for so long that I couldn't tell if the code was
    correct or not. With memoization turned on, it became evident that my
    logic was working, it just couldn't get to an answer fast.

--- 2015/elixir/lib/day_7.ex
+++ 2015/elixir/lib/day_7.ex
@@ -1,7 +1,133 @@
 defmodule AdventOfCode2015.Day7 do
+  def circuit_description_to_circuit(lines) do
+    lines
+    |> Enum.map(&line_to_operation_and_target/1)
+    |> Enum.reduce(%{}, fn {operation, target}, acc ->
+      put_in(acc[target], operation)
+    end)
+  end
+
+  def line_to_operation_and_target(line) do
+    [raw_operation, target] =
+      line
+      |> String.split("->", trim: true)
+      |> Enum.map(&String.trim/1)
+
+    operation =
+      raw_operation
+      |> String.split(" ")
+      |> parse_operation()
+
+    {operation, target}
+  end
+
+  def parse_operation([a]), do: {:literal, a}
+  def parse_operation(["NOT", a]), do: {:not, a}
+  def parse_operation([a, "AND", b]), do: {:and, a, b}
+  def parse_operation([a, "OR", b]), do: {:or, a, b}
+  def parse_operation([a, "LSHIFT", b]), do: {:lshift, a, b}
+  def parse_operation([a, "RSHIFT", b]), do: {:rshift, a, b}
+
+  def num_or_apply(a, context) do
+    try do
+      String.to_integer(a)
+    rescue
+      ArgumentError -> access_and_apply(a, context)
+    else
+      number -> {ensure_unsigned_16bit(number), context}
+    end
+  end
+
+  def access_and_apply(target, context) do
+    if memoized = context.memo[target] do
+      {memoized, context}
+    else
+      target_value = context.circuit[target]
+      {value, recursive_context} = operation_apply(target_value, context)
+      new_context = put_in(recursive_context, [:memo, target], value)
+
+      {value, new_context}
+    end
+  end
+
+  def unary(fun, a, context) do
+    {a_val, new_context} = num_or_apply(a, context)
+    value = fun.(a_val)
+
+    {value, new_context}
+  end
+
+  def binary(fun, a, b, context) do
+    {a_val, new_context} = num_or_apply(a, context)
+    {b_val, new_context} = num_or_apply(b, new_context)
+
+    value = fun.(a_val, b_val)
+
+    {value, new_context}
+  end
+
+  def operation_apply({:literal, a}, context) do
+    num_or_apply(a, context)
+  end
+
+  def operation_apply({:not, a}, context) do
+    unary(&Bitwise.bnot/1, a, context)
+  end
+
+  def operation_apply({:and, a, b}, context) do
+    binary(&Bitwise.band/2, a, b, context)
+  end
+
+  def operation_apply({:or, a, b}, context) do
+    binary(&Bitwise.bor/2, a, b, context)
+  end
+
+  def operation_apply({:lshift, a, b}, context) do
+    binary(&Bitwise.bsl/2, a, b, context)
+  end
+
+  def operation_apply({:rshift, a, b}, context) do
+    binary(&Bitwise.bsr/2, a, b, context)
+  end
+
+  def ensure_unsigned_16bit(number) do
+    Bitwise.band(number, 0xFFFF)
+  end
+
   def part_1(lines) do
+    circuit = circuit_description_to_circuit(lines)
+
+    {value, _memo} =
+      access_and_apply(
+        _start = "a",
+        _context = %{
+          circuit: circuit,
+          memo: %{}
+        }
+      )
+
+    value
   end
 
   def part_2(lines) do
+    circuit = circuit_description_to_circuit(lines)
+    wire_a_result = part_1(lines)
+
+    modified_circuit =
+      put_in(
+        circuit["b"],
+        {:literal, Integer.to_string(wire_a_result)}
+      )
+
+    {value, _memo} =
+      access_and_apply(
+        _start = "a",
+        _context = %{
+          circuit: modified_circuit,
+          memo: %{}
+        }
+      )
+
+    value
   end
 end
--- 2015/elixir/test/day_7_test.exs
+++ 2015/elixir/test/day_7_test.exs
@@ -4,21 +4,51 @@ defmodule AdventOfCode2015.Day7Test do
 
   alias AdventOfCode2015.Day7
 
-  describe "part_1/1" do
-    test "description" do
-      lines = ~w(
-      )
+  describe "circuit_description_to_circuit/1" do
+    test "parses a circuit and returns a map with wire name and output" do
+      lines =
+        """
+        123 -> x
+        456 -> y
+        x AND y -> d
+        x OR y -> e
+        x LSHIFT 2 -> f
+        y RSHIFT 2 -> g
+        NOT x -> h
+        NOT y -> i
+        """
+        |> String.split("\n", trim: true)
 
-      assert Day7.part_1(lines) == :foobar
+      assert Day7.circuit_description_to_circuit(lines) ==
+               %{
+                 "d" => {:and, "x", "y"},
+                 "e" => {:or, "x", "y"},
+                 "f" => {:lshift, "x", "2"},
+                 "g" => {:rshift, "y", "2"},
+                 "h" => {:not, "x"},
+                 "i" => {:not, "y"},
+                 "x" => {:literal, "123"},
+                 "y" => {:literal, "456"}
+               }
     end
   end
 
-  describe "part_2/1" do
-    test "description" do
-      lines = ~w(
-      )
+  describe "part_1/1" do
+    test "parses a circuit, then follows wire 'a'" do
+      lines =
+        """
+        123 -> a
+        456 -> y
+        a AND y -> d
+        a OR y -> e
+        a LSHIFT 2 -> f
+        y RSHIFT 2 -> g
+        NOT a -> h
+        NOT y -> i
+        """
+        |> String.split("\n", trim: true)
 
-      assert Day7.part_2(lines) == :foobar
+      assert Day7.part_1(lines) == 123
     end
   end
 end
