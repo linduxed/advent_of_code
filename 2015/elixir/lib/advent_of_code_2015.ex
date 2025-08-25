dedd4e114f804b7e3b645de49eb25694e286173b
Mikael Muszynski <linduxed@gmail.com>
2025-08-25 14:20:10 +0200

    Change input file trimming to default to true
    Very rarely do I ever want trimming to be false, so let's invert how it
    works.

--- 2015/elixir/lib/advent_of_code_2015.ex
+++ 2015/elixir/lib/advent_of_code_2015.ex
@@ -1,7 +1,9 @@
 defmodule AdventOfCode2015 do
   def read_input_for_day(day_number, opts \\ []) do
+    trim? = opts[:trim] || true
+
     "input/day_#{day_number}.txt"
     |> File.read!()
-    |> String.split("\n", trim: opts[:trim])
+    |> String.split("\n", trim: trim?)
   end
 end
