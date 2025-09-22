cb2c1531934de9d8757bfc8de670f7e216d52dc5
Mikael Muszynski <linduxed@gmail.com>
2025-09-22 12:26:03 +0200

    2015 - day 8 - part 2

--- 2015/elixir/lib/day_8.ex
+++ 2015/elixir/lib/day_8.ex
@@ -13,6 +13,13 @@ defmodule AdventOfCode2015.Day8 do
     |> Enum.count()
   end
 
+  def encode(string) do
+    string
+    |> String.replace("\\", "\\\\")
+    |> String.replace("\"", "\\\"")
+    |> then(& "\"" <> &1 <> "\"")
+  end
+
   def part_1(lines) do
     code_character_count_sum =
       lines
@@ -28,6 +35,17 @@ defmodule AdventOfCode2015.Day8 do
   end
 
   def part_2(lines) do
-    lines
+    original_character_count_sum =
+      lines
+      |> Enum.map(&code_character_count/1)
+      |> Enum.sum()
+
+    encoded_character_count_sum =
+      lines
+      |> Enum.map(&encode/1)
+      |> Enum.map(&code_character_count/1)
+      |> Enum.sum()
+
+    encoded_character_count_sum - original_character_count_sum
   end
 end
--- 2015/elixir/test/day_8_test.exs
+++ 2015/elixir/test/day_8_test.exs
@@ -30,6 +30,23 @@ defmodule AdventOfCode2015.Day8Test do
     end
   end
 
+  describe "encode/1" do
+    for {string, expected} <- [
+          {~S(""), "\"\\\"\\\"\""},
+          {~S("aaa"), "\"\\\"aaa\\\"\""},
+          {"\"aaa\\\"aaa\"", "\"\\\"aaa\\\\\\\"aaa\\\"\""},
+          {"\"\\x27\"", "\"\\\"\\\\x27\\\"\""},
+          {
+            "\"byc\\x9dyxuafof\\\\\\xa6uf\\\\axfozomj\\\\olh\\x6a\"",
+            "\"\\\"byc\\\\x9dyxuafof\\\\\\\\\\\\xa6uf\\\\\\\\axfozomj\\\\\\\\olh\\\\x6a\\\"\"",
+          }
+        ] do
+      test "#{string}" do
+        assert Day8.encode(unquote(string)) == unquote(expected)
+      end
+    end
+  end
+
   describe "part_1/1" do
     test "returns total amount of code characters minus characters in text" do
       lines =
@@ -41,6 +58,20 @@ defmodule AdventOfCode2015.Day8Test do
         ]
 
       assert Day8.part_1(lines) == 12
+    end
+  end
+
+  describe "part_2/1" do
+    test "returns difference between amount of code characters minus encoded characters" do
+      lines =
+        [
+          ~S(""),
+          ~S("aaa"),
+          ~S("aaa\"aaa"),
+          ~S("\x27")
+        ]
+
+      assert Day8.part_2(lines) == 19
     end
   end
 end
